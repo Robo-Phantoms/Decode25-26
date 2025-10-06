1e4bf2aea029f1453bbbd792c6d448107460f9a4
Robo-Phantoms <robophantoms@gmail.com>
2025-10-06 15:56:46 -0500

    Finally added Vel pid

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Subsystems/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Subsystems/Outtake.java
@@ -4,8 +4,11 @@ package org.firstinspires.ftc.teamcode.util.Subsystems;
 import com.acmerobotics.dashboard.config.Config;
 
 import dev.nextftc.control.ControlSystem;
+import dev.nextftc.control.feedback.PIDCoefficients;
+import dev.nextftc.control.feedforward.BasicFeedforwardParameters;
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.ftc.ActiveOpMode;
 import dev.nextftc.hardware.controllable.MotorGroup;
 import dev.nextftc.hardware.controllable.RunToVelocity;
 import dev.nextftc.hardware.impl.MotorEx;
@@ -22,22 +25,26 @@ public class Outtake implements Subsystem {
     private MotorEx flywheelRight = new MotorEx("flywheelRight");
     private MotorGroup flyWheelOuttake = new MotorGroup(flywheelRight, flywheelLeft);
     private ServoEx bumper = new ServoEx("bumper");
-    public static double kp, ki, kd;
-    public static double kv, ka, ks;
-    public static double targetVel = 0;
+    public static double kp = 0.001, ki = 0, kd = 0;
+    public static double kv=0.00035, ka=0, ks=0;
+    public static double targetVel=1500;
+    public static PIDCoefficients coefficients = new PIDCoefficients(kp, ki, kd);
+    public static BasicFeedforwardParameters ff = new BasicFeedforwardParameters(kv, ka, ks);
     private ControlSystem controller = ControlSystem.builder()
-            .velPid(kp, ki, kd)
-            .basicFF(kv, ka, ks)
+            .velPid(coefficients)
+            .basicFF(ff)
             .build();
 
-    public Command shootArtifact = new RunToVelocity(controller, targetVel).requires(this);
-    public Command stopShooting = new RunToVelocity(controller, 0).requires(this);
+    public Command shootArtifact = new RunToVelocity(controller, targetVel).requires(flyWheelOuttake);
+    public Command stopShooting = new RunToVelocity(controller, 0).requires(flyWheelOuttake);
     public Command moveBumper = new SetPosition(bumper, 0.5).requires(bumper);
     public Command reverseBumper = new SetPosition(bumper, 0).requires(bumper);
 
     @Override
     public void periodic(){
         flyWheelOuttake.setPower(controller.calculate(flyWheelOuttake.getState()));
+        ActiveOpMode.telemetry().addData("currentVel", flyWheelOuttake.getVelocity());
+        ActiveOpMode.telemetry().update();
     }
 
 }
